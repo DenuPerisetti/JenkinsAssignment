176a646f24a1f3994dbf0e52fce6937fa24bd7df
DenuPerisetti <pdsnsmrudula99@gmail.com>
2023-05-09 19:08:05 +0530

    Successfully commited

---
+++ SecondOne.java
@@ -0,0 +1,10 @@
+
+public class SecondOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Wipro");
+
+	}
+
+}
---
+++ ThirdOne.java
@@ -0,0 +1,10 @@
+
+public class ThirdOne {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Jenkins");
+		// TODO Auto-generated method stub
+
+	}
+
+}
